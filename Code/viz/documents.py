c5c0fa2db6a433ba85807fa40fd83a62f7815039
theokott <theoakott@gmail.com>
2017-12-08 17:48:41 +0000

    Cleaned up code and added caching for documents

---
+++ Code/viz/documents.py
@@ -0,0 +1,32 @@
+# TODO:
+#       Enumerations for type
+
+
+class Reference:
+    def __init__(self):
+        self.id = -1
+        self.source = ""
+        self.target = ""
+        self.type = -1
+
+    def set_source(self, source):
+        self.source = source
+
+    def set_target(self, target):
+        self.target = target
+
+    def set_type(self, ref_type):
+        self.type = ref_type
+
+
+class RFC:
+    def __init__(self, id):
+        self.id = id
+        self.draft_name = ""
+        self.draft_url = ""
+
+    def set_draft_name(self, name):
+        self.draft_name = name
+
+    def set_draft_url(self, url):
+        self.draft_url = url
--- Code/viz/rfcInfo.py
+++ Code/viz/rfcInfo.py
@@ -1,58 +1,75 @@
 # TODO:
 #      Very slow
+#           - Twisted (framework)
+#           - Cache stuff
 #      Better structure for docs, colors and labels
+#           - Make interesting and useful
 #      Is the graph accurate?
 #      Is it a tree?
+#      Clean up code (PEP8)
 
 import requests as rq
 import networkx as nx
 import matplotlib.pyplot as plt
-import Doc as doc
-import Reference as ref
+import documents as doc
 
 base = 'https://datatracker.ietf.org'
+doc_cache = {}
+cached_calls = 0
 
-def getDocUrl(rfcNum):
-    resp = rq.get(base + '/api/v1/doc/docalias/?name=' + rfcNum)
+
+def get_doc_url(rfc_num):
+    resp = rq.get(base + '/api/v1/doc/docalias/?name=' + rfc_num)
     body = resp.json().get('objects')[0]
 
     return body.get('document')
 
-def getName(docUrl):
-    resp = rq.get(base + docUrl)
+
+def get_name(doc_url):
+    resp = rq.get(base + doc_url)
     body = resp.json()
 
     return body.get('name')
 
-def getRelationships(docName):
-    resp = rq.get(base + '/api/v1/doc/relateddocument/?source=' + docName)
+
+def get_relationships(doc_name):
+    resp = rq.get(base + '/api/v1/doc/relateddocument/?source=' + doc_name)
     relationships = resp.json().get('objects')
 
     return relationships
 
-def generateDoc(rfcNum):
-    newDoc = doc.RFC(rfcNum)
-    newDoc.set_draft_url(getDocUrl(newDoc.id))
-    newDoc.set_draft_name(getName(newDoc.draft_url))
 
-    return newDoc
+def get_doc(rfc_num):
+    new_doc = doc.RFC(rfc_num)
+    new_doc.set_draft_url(get_doc_url(new_doc.id))
+    new_doc.set_draft_name(get_name(new_doc.draft_url))
 
-def generateEdges(G, root):
-    return G
+    return new_doc
+
 
 def find_related_docs(G, root, level):
-
+    global cached_calls
     if level == 0:
         return
 
     references = []
-    relationships = getRelationships(root.draft_name)
+    relationships = get_relationships(root.draft_name)
     for relationship in relationships:
-        new_reference = ref.Reference()
+        new_reference = doc.Reference()
         new_reference.set_source(root)
 
         target_split = relationship.get('target').split('/')
-        target_doc = generateDoc(target_split[-2].upper())
+        target_doc_id = target_split[-2].upper()
+
+        if target_doc_id in doc_cache:
+            print(target_doc_id, " is cached")
+            target_doc = doc_cache[target_doc_id]
+            cached_calls = cached_calls + 1
+        else:
+            print(target_doc_id, " is not cached")
+            target_doc = get_doc(target_split[-2].upper())
+            doc_cache[target_doc.id] = target_doc
+
         new_reference.set_target(target_doc)
 
         type_split = relationship.get('relationship').split('/')
@@ -62,7 +79,7 @@ def find_related_docs(G, root, level):
 
     for reference in references:
 
-        if (reference.type == 'refold'):
+        if reference.type == 'refold':
                 G.add_edge(reference.source.id, reference.target.id, relType=reference.type, color='g')
         else:
             G.add_edge(reference.source.id, reference.target.id, relType=reference.type, color='b')
@@ -70,7 +87,7 @@ def find_related_docs(G, root, level):
         find_related_docs(G, reference.target, level - 1)
 
 
-def drawGraph(G):
+def draw_graph(G):
     labels = {}
 
     for node in G.nodes:
@@ -83,24 +100,34 @@ def drawGraph(G):
     plt.axis('off')
     plt.show()
 
+
 def main():
     G = nx.Graph()
 
-    rfcNum = 'RFC' + input('Enter the requested RFC number: ')
-    rootDoc = generateDoc(rfcNum)
+    rfc_num = 'RFC' + input('Enter the requested RFC number: ')
+    deg_of_separation = input('Enter the degree of separation: ')
 
-    G.add_node(rootDoc)
+    root_doc = get_doc(rfc_num)
 
-    print('\n' + ('=' * len(rootDoc.draft_name)))
-    print(rootDoc.draft_name)
-    print(('=' * len(rootDoc.draft_name)) + '\n')
+    G.add_node(root_doc)
+    doc_cache[root_doc.id] = root_doc
+
+    print('\n' + ('=' * len(root_doc.draft_name)))
+    print(root_doc.draft_name)
+    print(('=' * len(root_doc.draft_name)) + '\n')
     print('Building graph...')
 
-    find_related_docs(G, rootDoc, 2)
+    find_related_docs(G, root_doc, int(deg_of_separation))
 
     print('Drawing graph...')
 
-    drawGraph(G)
-    nx.write_dot(G, "graph.dot")
+    draw_graph(G)
 
 main()
+
+for x in doc_cache:
+    print(x)
+
+total_calls = cached_calls + len(doc_cache.keys())
+
+print("total calls: ", total_calls, " calls saved: ", cached_calls, " ", (cached_calls/total_calls)*100, "%")
